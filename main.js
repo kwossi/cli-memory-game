9d8ef5a242ed94bd40db01f818589b1b03e3abc2
kwossi <krissi.stoecker@gmail.com>
2024-05-21 18:32:06 +0200

    changed colors other changes

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 import readlineSync from "readline-sync";
 import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 
 //  MEMORY GAME FOR 2 PLAYERS
+// test mode, change line  20 to 0
+
+// Colors
+
+let color1 = "#3EB489"; //former magenta :> mint
+let color2 = "#C71585"; // former yellow :> purple
+let color3 = "#FFD700"; // former cyan :> yellow
+let color4 = "#32012F"; // dark purple for text
 
 //  Classes
 
 class Player {
   constructor(name) {
     this.name = name;
-    this.score = 0;
+    this.score = 7.5;
   }
   scoreOne() {
     this.score++;
@@ -26,28 +35,31 @@ class Board {
   }
   showBoard() {
     console.log(
-      `\t\t   1\t   2\t   3\t   4\t   5\n\n
+      chalk.hex(color3)(`\t\t   1\t   2\t   3\t   4\t   5\n\n
       A:\t${this.a[0].display} ${this.a[1].display} ${this.a[2].display} ${this.a[3].display} ${this.a[4].display}\n
       B:\t${this.b[0].display} ${this.b[1].display} ${this.b[2].display} ${this.b[3].display} ${this.b[4].display}\n
       C:\t${this.c[0].display} ${this.c[1].display} ${this.c[2].display} ${this.c[3].display} ${this.c[4].display}\n
       D:\t${this.d[0].display} ${this.d[1].display} ${this.d[2].display} ${this.d[3].display} ${this.d[4].display}\n
       E:\t${this.e[0].display} ${this.e[1].display} ${this.e[2].display} ${this.e[3].display} ${this.e[4].display}\n
-      F:\t${this.f[0].display} ${this.f[1].display} ${this.f[2].display} ${this.f[3].display} ${this.f[4].display}\n`
+      F:\t${this.f[0].display} ${this.f[1].display} ${this.f[2].display} ${this.f[3].display} ${this.f[4].display}\n`)
     );
   }
 }
 
 class Tile {
-  constructor(symbol, covered = chalk.bgYellow("[     ]")) {
-    this.symbol = chalk.bgGreen(`[  ${symbol}  ]`);
+  constructor(symbol, covered = chalk.bgHex(color1)("[     ]")) {
+    this.symbol = chalk.bgHex(color2)(`[  ${symbol}  ]`);
+    this.isRevealed = false;
     this.display = covered;
     this.covered = covered;
   }
-  revealTile() {
+  showTile() {
     this.display = this.symbol;
+    this.isRevealed = true;
   }
   coverTile() {
     this.display = this.covered;
+    this.isRevealed = false;
   }
 }
 
@@ -67,23 +79,45 @@ class Game {
   welcomeScreen() {
     console.clear();
     console.log(
-      `\tWelcome to Memor-ICE\n\n\n\t\tThe coolest memory game for 2 players.\n\n\n`
+      chalk.hex(color3)(
+        `\tWelcome to \n\t${chalk.bgHex(color1)(
+          `                                  `
+        )}\n\t${chalk.bgHex(color2)(
+          `                       ${chalk.hex(color1)("MEMOR-ICE")}  `
+        )}\n\t\tThe coolest memory game for\n\t\t${chalk.bgHex(color1)(
+          `                                                       `
+        )}\n\t\t${chalk.bgHex(color2)(
+          `                                           ${chalk.hex(color1)(
+            "2 PLAYERS."
+          )}  `
+        )}\n`
+      )
     );
-    readlineSync.question(`\t\t\tPress enter to play!`);
+    readlineSync.question(`\t\t\t${chalk.hex(color1)("Press enter to play!")}`);
   }
 
   showRules() {
     console.clear();
     console.log(
-      `\tRULES\n\n- Players take turns flipping over two cards, one at a time, trying to find a matching pair.\n\n- If you find a matching pair, you score a point and go again!\n\n- The player with the most points wins!\n\n`
+      chalk.hex(color4)(
+        `\t${chalk.bgHex(color1)(
+          "       • R U L E S •                    "
+        )}\n\t${chalk.bgHex(color2)(
+          "                                        "
+        )}\n\n${chalk.bgHex(color3)(
+          `\t• Players take turns flipping over two cards, \n\tone at a time, trying to find a matching pair.\n\n\t• If you find a matching pair, you score a point \n\tand go again!\n\n\t• The player with the most points wins!`
+        )}\n\n`
+      )
     );
-    readlineSync.question(`Understood?`);
+    readlineSync.question(chalk.hex(color1)(`\t\t\tUnderstood!`));
     console.clear();
   }
 
   createPlayer(which) {
-    let name = readlineSync.question(`${which}, please enter your name: `);
-    console.log(`Thank you ${name}! Please take a seat!`);
+    let name = readlineSync.question(
+      chalk.hex(color1)(`\t${which}, please enter your name: `)
+    );
+    console.log(chalk.hex(color3)(`\tThank you ${name}! Please take a seat!`));
     return new Player(name);
   }
 
@@ -119,7 +153,17 @@ class Game {
   //   game play functions
   showScore() {
     console.log(
-      `Score:\n\n${this.player1.name}: ${this.player1.score}\t${this.player2.name}: ${this.player2.score}\n\n`
+      chalk.hex(color4)(
+        `\n\n\t${chalk.bgHex(color1)(
+          "Score:                       "
+        )}\n\t${chalk.bgHex(color2)(
+          "                             "
+        )}\n\t${chalk.bgHex(color3)(this.player1.name)} ${chalk.white(
+          this.player1.score
+        )}\t${chalk.bgHex(color3)(this.player2.name)} ${chalk.white(
+          this.player2.score
+        )}\n\n`
+      )
     );
   }
 
@@ -136,12 +180,14 @@ class Game {
   }
 
   congratWinner() {
+    let winner = this.getWinner();
     console.log(
-      `The final score is:\n${this.player1.name}: ${this.player1.score} vs. ${
-        this.player2.name
-      }: ${
-        this.player2.score
-      }\nCongratulations ${this.getWinner()}! You won the game!`
+      chalk.hex(color1)(
+        `\tThe final score is:\n\t${this.player1.name}: ${this.player1.score} vs. ${this.player2.name}: ${this.player2.score}\n\tCongratulations`
+      ) +
+        `\n\t\t` +
+        chalkAnimation.rainbow(winner) +
+        chalk.hex(color1)`\n\tYou won the game!`
     );
   }
 
@@ -155,10 +201,12 @@ class Game {
   }
 
   verifyTile(tile) {
-    if (tile && tile.display !== "\x1B[43m[     ]\x1B[49m") {
-      readlineSync.question(`Already revealed. Choose a different tile!`);
+    if (tile && tile.isRevealed) {
+      readlineSync.question(
+        chalk.hex(color3)(`\tAlready revealed. Choose a different tile!`)
+      );
     } else {
-      return tile.display === "\x1B[43m[     ]\x1B[49m";
+      return tile && tile.isRevealed !== true;
     }
   }
 
@@ -167,7 +215,7 @@ class Game {
     if (this.board?.[row]?.[+col - 1]) {
       return this.board[row][+col - 1];
     } else {
-      console.log(`Invalid input. Try again!`);
+      console.log(chalk.hex(color3)(`\tInvalid input. Try again!`));
       return false;
     }
   }
@@ -178,23 +226,31 @@ class Game {
     do {
       currentTileA = this.getTile(
         readlineSync.question(
-          `It's ${this.currentPlayer.name}'s turn. Choose your first tile ( eg. a5): `
+          chalk.hex(color1)(
+            `\tIt's ${chalk.hex(color4).bgHex(color3)(
+              this.currentPlayer.name
+            )}'s turn. Choose your first tile ( eg. a5): `
+          )
         )
       );
     } while (!currentTileA || !this.verifyTile(currentTileA));
-    currentTileA.revealTile();
+    currentTileA.showTile();
     this.displayAll();
     do {
       currentTileB = this.getTile(
         readlineSync.question(
-          `${this.currentPlayer.name}, please choose your second tile ( eg. c2): `
+          chalk.hex(color1)(
+            `\t${chalk.hex(color4).bgHex(color3)(
+              this.currentPlayer.name
+            )}, please choose your second tile ( eg. c2): `
+          )
         )
       );
       if (currentTileA === currentTileB) {
-        readlineSync.question(`Already revealed. Choose a different tile!`);
+        readlineSync.question(`\tAlready chosen. Choose a different tile!`);
       }
     } while (!this.verifyTile(currentTileB) || currentTileA === currentTileB);
-    currentTileB.revealTile();
+    currentTileB.showTile();
     this.displayAll();
     return [currentTileA, currentTileB];
   }
@@ -202,11 +258,17 @@ class Game {
   checkForMatch(currentTiles) {
     if (currentTiles[0].symbol === currentTiles[1].symbol) {
       readlineSync.question(
-        `It's a match!! ${this.currentPlayer.name} scores + 1 and goes again!`
+        chalk.hex(color1)(
+          `\tIt's a match!! ${chalk.hex(color4).bgHex(color3)(
+            this.currentPlayer.name
+          )} scores + 1 and goes again!`
+        )
       );
       this.currentPlayer.scoreOne();
     } else {
-      readlineSync.question(`Sorry, no match :( Next Player!`);
+      readlineSync.question(
+        chalk.hex(color1)(`\tSorry, no match :( Next Player!`)
+      );
       currentTiles[0].coverTile();
       currentTiles[1].coverTile();
       this.updateTurnCount();
@@ -217,7 +279,13 @@ class Game {
   oneTurn() {
     this.displayAll();
     this.currentPlayer = this.updateCurrentPlayer();
-    readlineSync.question(`Ready ${this.currentPlayer.name}? Press enter`);
+    readlineSync.question(
+      chalk.hex(color1)(
+        `\tReady ${chalk.hex(color4).bgHex(color3)(
+          this.currentPlayer.name
+        )}? Press enter! `
+      )
+    );
     this.displayAll();
     let currentTiles = this.getInputs();
     this.checkForMatch(currentTiles);
